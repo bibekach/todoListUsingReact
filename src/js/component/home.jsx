c792af8552914102e2e51b2687464ff5f7a67929
Bibek Acharya <acharya.bbk@gmail.com>
2023-04-25 22:27:50 +0000

    completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,30 +8,34 @@ const Home = () => {
 	const [deleteBottom, setdeleteBottom] = useState(false);
 
 	const remove = (index) => {
-		setList(list.filter((item , y) => y != index));
+		setList(list.filter((item, y) => y != index));
 	}
 
 	return (
 		<div className="container">
 			<div className="row justify-content-center">
-				<div className="col-12 col-md-8 mt-1 list-group-item">
-					<input value={newTask} placeholder="Add new task" onChange={(e) => { setNewTask(e.target.value) }}
-						onKeyUp={(e) => {
-							if (e.key == "Enter") {
-								setList([...list, newTask])
-								setNewTask("");
-
-							}
-						}} />
-					<ul className="list-group">
-						{
-							list.map((todo, index) => {
-								return <li className="list-group-item w-50" key={index}>{todo} <div className="justify-content-end bg-danger" onClick={() => {
-									remove(index);
-								}}>x</div></li>
-							})
+				<div className="col-12 list-group-item">
+					<input className="form-control" value={newTask} placeholder="Add new task" onChange={(e) => { setNewTask(e.target.value) }} onKeyUp={(e) => {
+						if (e.key == "Enter") {
+							setList([...list, newTask])
+							setNewTask("");
 						}
-					</ul>
+					}} />
+					<div className="row d-flex">
+						<ul className="list-group d-flex justify-content-between">
+							{
+								list.map((todo, index) => {
+									return <li className="list-group-item d-flex" key={index}>{todo}
+
+										<button type="button" class="ms-auto btn btn-danger" onClick={() => {
+											remove(index);
+										}}>DELETE</button>
+
+									</li>
+								})
+							}
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
